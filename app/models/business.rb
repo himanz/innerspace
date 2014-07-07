105d594a8d7c2a678334098db458ee0fb4ca8c4e
Jonathan Chang <jonoschang7@gmail.com>
2014-07-07 16:15:04 -0400

    Make change to by_letter to allow sort by letter

--- app/models/business.rb
+++ app/models/business.rb
@@ -3,6 +3,6 @@ class Business < ActiveRecord::Base
 	validates :address, presence: true
 
 	def self.by_letter(letter)
-		where("name LIKE ?", "#{letter}").order(:name)
+		where("name LIKE ?", "#{letter}%").order(:name)
 	end
 end
