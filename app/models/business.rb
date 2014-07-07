431c5771fbc67fef5328f782e42fda81aa0ff74b
Jonathan Chang <jonoschang7@gmail.com>
2014-07-07 16:07:51 -0400

    create by_letter method

--- app/models/business.rb
+++ app/models/business.rb
@@ -2,7 +2,7 @@ class Business < ActiveRecord::Base
 	validates :name, presence: true
 	validates :address, presence: true
 
-	# def self.by_letter(letter)
-	# 	where("name LIKE ?", "#{letter}").order(:name)
-	# end
+	def self.by_letter(letter)
+		where("name LIKE ?", "#{letter}").order(:name)
+	end
 end
