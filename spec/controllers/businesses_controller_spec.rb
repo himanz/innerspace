ff02772c794d9136ddbf53426eb1ca54b5ee1593
Jonathan Chang <jonoschang7@gmail.com>
2014-07-07 15:40:59 -0400

    add tests to get #index of businesses controller

--- spec/controllers/businesses_controller_spec.rb
+++ spec/controllers/businesses_controller_spec.rb
@@ -1,5 +1,16 @@
 require 'spec_helper'
 
 describe BusinessesController do
+	describe 'GET #index' do
+		context 'with params[:letter]' do
+			it "populates an array of businesses starting with the letter"
+			it "renders the :index view"
+		end
+
+		context 'without params[:letter]' do
+			it "populates an array of all businesses"
+			it "renders the :index view"
+		end
+	end
 
 end
