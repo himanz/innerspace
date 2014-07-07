dc839544d627c30f8db7fd9318db51f4b0a85a05
Jonathan Chang <jonoschang7@gmail.com>
2014-07-07 15:53:49 -0400

    write test for populates array with letter

--- spec/controllers/businesses_controller_spec.rb
+++ spec/controllers/businesses_controller_spec.rb
@@ -3,7 +3,11 @@ require 'spec_helper'
 describe BusinessesController do
 	describe 'GET #index' do
 		context 'with params[:letter]' do
-			it "populates an array of businesses starting with the letter"
+			it "populates an array of businesses starting with the letter" do
+				pho = create(:business, name: 'Pho')
+				bob = create(:business, name: 'Bob')
+				expect(assigns(:businesses)).to match_array([pho])
+			end
 			it "renders the :index view"
 		end
 
