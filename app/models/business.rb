42180dbf391c7d3f01393892941b67daa38cffa9
Jonathan Chang <jonoschang7@gmail.com>
2014-07-07 14:25:51 -0400

    validate presence of name and address of business model

--- app/models/business.rb
+++ app/models/business.rb
@@ -1,2 +1,4 @@
 class Business < ActiveRecord::Base
+	validates :name, presence: true
+	validates :address, presence: true
 end
--- spec/models/business_spec.rb
+++ spec/models/business_spec.rb
@@ -1,7 +1,18 @@
 require 'spec_helper'
 
 describe Business do
-  it "is valid with a name and address"
-  it "is invalid without a name"
-  it "is invalid without an address"
+  it "is valid with a name and address" do
+  	business = Business.new(
+  		name: 'Pho Paradise',
+  		address: '100 McCowan Road')
+  	expect(business).to be_valid
+  end
+
+  it "is invalid without a name" do
+  	expect(Business.new(name: nil)).to have(1).errors_on(:name)
+  end
+
+  it "is invalid without an address" do
+  	expect(Business.new(address: nil)).to have(1).errors_on(:address)
+  end
 end
