f33a5d7814bb19f9e4bd0e8d61b2dd77f79530d2
Jonathan Chang <jonoschang7@gmail.com>
2014-07-08 15:46:19 -0400

    add cbp to seed business

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,16 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Business.create(name: "Sangri-La Hotel", address: "188 University Ave", heading: 200, pano: "3L5NCiIf__oAAAQIt-clrA")
-Business.create(name: "Search Engine People Inc.", address: "1305 Pickering Pkwy #500", heading: 300, pano: "JSuPKuDL7l2DyHx0jF4nmg")
+Business.create(name: "Sangri-La Hotel", 
+	              address: "188 University Ave", 
+	              heading: 200, 
+	              pano: "3L5NCiIf__oAAAQIt-clrA",
+	              cbp: ",191.0,,0,-0.0"
+	              )
+
+Business.create(name: "Search Engine People Inc.",
+                address: "1305 Pickering Pkwy #500",
+                heading: 300,
+                pano: "JSuPKuDL7l2DyHx0jF4nmg",
+                cbp: ",316.42,,0,-0.0"
+                )
