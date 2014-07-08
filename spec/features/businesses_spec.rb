c2f5275c93ba5e3279d243e3848c514a4b4151e2
Jonathan Chang <jonoschang7@gmail.com>
2014-07-08 00:50:55 -0400

    refactor add a new business

--- spec/features/businesses_spec.rb
+++ spec/features/businesses_spec.rb
@@ -6,24 +6,26 @@ feature 'Business management' do
 	end
 
 	scenario "add a new business" do		
-	  visit new_business_path
-	  fill_in 'Name', with: @default.name
-	  fill_in 'Address', with: @default.address
-	  click_button 'Submit'
+	  # visit new_business_path
+	  # fill_in 'Name', with: @default.name
+	  # fill_in 'Address', with: @default.address
+	  # click_button 'Submit'
 
 	  visit new_business_path
 	  expect {
-	  	fill_in 'Name', with: 'Default Cafe'
-	  	fill_in 'Address', with: '1 Default Drive'
+	  	fill_in 'Name', with: @default.name
+	  	fill_in 'Address', with: @default.address
 	  	click_button 'Submit'
 	  }.to change(Business, :count).by(1)
-	  expect(current_path).to eq business_path((@default.id + 2))
+	  expect(current_path).to eq business_path((@default.id + 1))
 	  expect(page).to have_content "Business was successfully created"
 	  within 'h1' do
 	  	expect(page).to have_content 'Default Cafe'
 	  end
 	end
 
-	scenario "edit a business"
+	scenario "edit a business" do
+		# visit edit_business_path(@default)
+	end
 	scenario "delete a business"
 end
