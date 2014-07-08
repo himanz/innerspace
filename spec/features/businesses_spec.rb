e5a22d50a23ed2852ca11360ac7db884223b7f72
Jonathan Chang <jonoschang7@gmail.com>
2014-07-08 00:59:58 -0400

    test spec for editing a business

--- spec/features/businesses_spec.rb
+++ spec/features/businesses_spec.rb
@@ -25,7 +25,17 @@ feature 'Business management' do
 	end
 
 	scenario "edit a business" do
-		# visit edit_business_path(@default)
+		visit edit_business_path(@default)
+		expect{
+		  fill_in 'Name', with: 'Yum Bar'
+		  click_button 'Submit'
+		}.to_not change(Business, :count)
+		expect(current_path).to eq business_path(@default.id)
+		expect(page).to have_content "Business was successfully updated."
+		within 'h1' do
+			expect(page).to have_content 'Yum Bar'
+			expect(page).to have_content '1 Default Drive'
+		end
 	end
 	scenario "delete a business"
 end
