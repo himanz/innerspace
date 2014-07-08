a20a48b4d8886fba5bdc8af8995b478114fb6684
Jonathan Chang <jonoschang7@gmail.com>
2014-07-08 17:27:38 -0400

    Test spec for category invalid without name

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -5,4 +5,8 @@ describe Category do
   	category = create(:category)
   	expect(category).to be_valid
   end
+
+  it "is invalid without a name" do
+  	expect(Category.new(name: nil)).to have(1).errors_on(:name)
+  end
 end
