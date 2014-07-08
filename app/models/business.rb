d656d4c3ef48b351a40882886cc3611d8d421de2
Jonathan Chang <jonoschang7@gmail.com>
2014-07-08 14:34:55 -0400

    validate presence of pano and heading

--- app/models/business.rb
+++ app/models/business.rb
@@ -1,6 +1,8 @@
 class Business < ActiveRecord::Base
 	validates :name, presence: true
 	validates :address, presence: true
+	validates :pano, presence: true
+	validates :heading, presence: true
 
 	def self.by_letter(letter)
 		where("name LIKE ?", "#{letter}%").order(:name)
