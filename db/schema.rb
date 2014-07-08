24f817597c67303b57474a628ad90fc577709371
Jonathan Chang <jonoschang7@gmail.com>
2014-07-08 17:17:18 -0400

    generate category model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140708193847) do
+ActiveRecord::Schema.define(version: 20140708211429) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,12 @@ ActiveRecord::Schema.define(version: 20140708193847) do
     t.string   "pano"
     t.decimal  "heading"
     t.string   "cbp"
+  end
+
+  create_table "categories", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
