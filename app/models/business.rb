15e5f3436ca3419522e2395d9d0ac5760f9d63ed
Jonathan Chang <jonoschang7@gmail.com>
2014-07-08 17:57:48 -0400

    add belongs_to and has_many assocation, validation for category

--- app/models/business.rb
+++ app/models/business.rb
@@ -1,9 +1,13 @@
 class Business < ActiveRecord::Base
+	belongs_to :pin
+
 	validates :name, presence: true
 	validates :address, presence: true
 	validates :pano, presence: true
 	validates :heading, presence: true
 	validates :cbp, presence: true
+  validates :category, presence: true
+
 
 	def self.by_letter(letter)
 		where("name LIKE ?", "#{letter}%").order(:name)
