0ac8da994b5e8b40a91331036c97ff707239b222
Jonathan Chang <jonoschang7@gmail.com>
2014-07-08 00:46:19 -0400

    put @default in a before each block

--- spec/features/businesses_spec.rb
+++ spec/features/businesses_spec.rb
@@ -1,12 +1,14 @@
 require 'spec_helper'
 
 feature 'Business management' do
-	scenario "add a new business" do
-		default = create(:default)
+	before :each do
+		@default = create(:default)
+	end
 
+	scenario "add a new business" do		
 	  visit new_business_path
-	  fill_in 'Name', with: default.name
-	  fill_in 'Address', with: default.address
+	  fill_in 'Name', with: @default.name
+	  fill_in 'Address', with: @default.address
 	  click_button 'Submit'
 
 	  visit new_business_path
@@ -15,7 +17,7 @@ feature 'Business management' do
 	  	fill_in 'Address', with: '1 Default Drive'
 	  	click_button 'Submit'
 	  }.to change(Business, :count).by(1)
-	  expect(current_path).to eq business_path((default.id + 2))
+	  expect(current_path).to eq business_path((@default.id + 2))
 	  expect(page).to have_content "Business was successfully created"
 	  within 'h1' do
 	  	expect(page).to have_content 'Default Cafe'
