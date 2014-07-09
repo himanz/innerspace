9550328da3cef7bcea0d8eed382f65733805c185
Jonathan Chang <jonoschang7@gmail.com>
2014-07-08 22:38:48 -0400

    add routes for categories

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Innerspace::Application.routes.draw do
   resources :businesses
+  resources :categories
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
