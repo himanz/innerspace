53ddf95d7443c9326a1b7620030c21eb04d9784d
Jonathan Chang <jonoschang7@gmail.com>
2014-07-08 22:37:58 -0400

    test for populates an array of categories in alphabetical order

--- spec/controllers/categories_controller_spec.rb
+++ spec/controllers/categories_controller_spec.rb
@@ -2,7 +2,12 @@ require 'spec_helper'
 
 describe CategoriesController do
   describe 'GET #index' do
-    it "populates an array of categories in alphabetical order" 
+    it "populates an array of categories in alphabetical order" do
+    	hotel = create(:category, name: 'Hotel')
+    	bar = create(:category, name: 'Bar')
+    	get :index
+    	expect(assigns(:categories)).to match_array([bar, hotel])
+    end
 
     it "renders the :index view"
   end
