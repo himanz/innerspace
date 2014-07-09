5f5811d7c1d060009eac2b2bee7e385e659903b2
Jonathan Chang <jonoschang7@gmail.com>
2014-07-09 02:12:39 -0400

    add edit to set_category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,5 @@
 class CategoriesController < ApplicationController
-	before_action :set_category, only: [:show]
+	before_action :set_category, only: [:show, :edit]
 
 	def index
 		@categories = Category.order("name")
