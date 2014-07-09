23904a37c0add228527f9442f56bdf3a7deabd44
Jonathan Chang <jonoschang7@gmail.com>
2014-07-08 23:38:08 -0400

    create set_category method for find category by id

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,10 +1,16 @@
 class CategoriesController < ApplicationController
-	
+	before_action :set_category, only: [:show]
+
 	def index
 		@categories = Category.order("name")
 	end
 
 	def show
+	end
 
+	private
+
+	def set_category
+		@category = Category.find(params[:id])
 	end
 end
