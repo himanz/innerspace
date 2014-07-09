3e366015366b55a0de5055823edad5f9ad1c2b68
Jonathan Chang <jonoschang7@gmail.com>
2014-07-08 22:34:24 -0400

    describe GET #index tests for categories controller

--- spec/controllers/categories_controller_spec.rb
+++ spec/controllers/categories_controller_spec.rb
@@ -1,5 +1,9 @@
 require 'spec_helper'
 
 describe CategoriesController do
+  describe 'GET #index' do
+    it "populates an array of categories in alphabetical order" 
 
+    it "renders the :index view"
+  end
 end
