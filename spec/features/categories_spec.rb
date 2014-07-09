8e72d135e93f31702504035cfbd458b9e044bb83
Jonathan Chang <jonoschang7@gmail.com>
2014-07-09 15:10:06 -0400

    create default category in before each block

--- spec/features/categories_spec.rb
+++ spec/features/categories_spec.rb
@@ -1,6 +1,10 @@
 require 'spec_helper'
 
 feature 'Category management' do
+	before :each do
+		@default = create(:category_default)
+	end
+	
 	scenario "add a new category"
 	scenario "edit a category"
 	scenario "delete a category"
