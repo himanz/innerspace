d231aec9b7cada98fcfcd99de711e96e4018e107
Jonathan Chang <jonoschang7@gmail.com>
2014-07-12 13:16:25 -0400

    test for user is invalid without an email

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,12 @@
 require 'spec_helper'
 
 describe User do
-  it "it is valid with a name and password" do
+  it "it is valid with an email and password" do
   	user = create(:user)
   	expect(user).to be_valid
+  end
+
+  it "is invalid without an email " do
+  	expect(User.new(email: nil)).to have(1).errors_on(:email)
   end
 end
