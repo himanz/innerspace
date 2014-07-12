9706f43cf985c3f20367129e3cac9650012bf3df
Jonathan Chang <jonoschang7@gmail.com>
2014-07-12 14:41:38 -0400

    move click link sign up to out of expect block

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -7,14 +7,16 @@ feature 'User management' do
 
 	scenario "user signup" do
 		visit root_path
+		click_link 'Sign up'
 		expect {
-			click_link 'Sign up'
+			
 			fill_in 'user_email', with: "joe@joe.com"
 			fill_in 'user_password', with: "12345678"
 			fill_in 'user_password_confirmation', with: "12345678"
 			click_button 'Sign up'
 		}.to change(User, :count).by(1)
 	end
+
 	scenario "edit a user"
 	scenario "user login"
 	scenario "user logout"
