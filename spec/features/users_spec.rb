94d388bf61cdd62c4c9c992caab3efd16d5513e3
Jonathan Chang <jonoschang7@gmail.com>
2014-07-12 14:49:44 -0400

    test for user login

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -9,15 +9,22 @@ feature 'User management' do
 		visit root_path
 		click_link 'Sign up'
 		expect {
-			
 			fill_in 'user_email', with: "joe@joe.com"
 			fill_in 'user_password', with: "12345678"
 			fill_in 'user_password_confirmation', with: "12345678"
 			click_button 'Sign up'
 		}.to change(User, :count).by(1)
 	end
-
+  
+  scenario "user login" do
+  	visit root_path
+  	click_link "Login"
+  	fill_in 'user_email', with: @default.email
+  	fill_in 'user_password', with: @default.password
+  	click_button 'Sign in'
+  	expect(page).to have_content "Signed in successfully."
+  end
+  scenario "user logout"
 	scenario "edit a user"
-	scenario "user login"
-	scenario "user logout"
+	
 end
