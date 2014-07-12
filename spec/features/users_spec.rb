11e0cc0f4f7e963d88251f1c40ef957246c5cdb9
Jonathan Chang <jonoschang7@gmail.com>
2014-07-12 14:19:49 -0400

    test for user sign up

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -1,7 +1,20 @@
 require 'spec_helper'
 
 feature 'User management' do
-	scenario "user signup"
+  before :each do
+  	@default = create(:default_user)
+  end
+
+	scenario "user signup" do
+		visit root_path
+		expect {
+			click_link 'Sign up'
+			fill_in 'user_email', with: @default.email
+			fill_in 'user_password', with: @default.password
+			fill_in 'user_password_confirmation', with: @default.password
+			click_button 'Sign up'
+		}.to change(User, :count).by(1)
+	end
 	scenario "edit a user"
 	scenario "user login"
 	scenario "user logout"
