39325e63c3a3b4a1562455c977c47c4632ce26e5
Jonathan Chang <jonoschang7@gmail.com>
2014-07-12 13:06:15 -0400

    factory for user

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,5 +2,18 @@
 
 FactoryGirl.define do
   factory :user do
+  	email "jon@jon.com"
+  	password "123456"
+  	password_confirmation "123456"
+
+  	factory :invalid_user do
+  		email nil
+  	end
+
+  	factory :default_user do
+  		email "default@default.com"
+  		password "default"
+  		password_confirmation "default"
+  	end
   end
 end
