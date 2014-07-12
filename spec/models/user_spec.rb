314e6a6f5bc58c22e26c3b41f35b9faf91927f94
Jonathan Chang <jonoschang7@gmail.com>
2014-07-12 13:17:53 -0400

    test for user is invalid without a password

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,7 +6,11 @@ describe User do
   	expect(user).to be_valid
   end
 
-  it "is invalid without an email " do
+  it "is invalid without an email" do
   	expect(User.new(email: nil)).to have(1).errors_on(:email)
+  end
+
+  it "is invalid without a password" do
+  	expect(User.new(password: nil)).to have(1).errors_on(:password)
   end
 end
