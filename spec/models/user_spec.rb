7ac20f7877dd0087032b9532ac63b75d788fcd33
Jonathan Chang <jonoschang7@gmail.com>
2014-07-12 13:22:30 -0400

    test for user is invalid with under 8 character password

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,4 +13,8 @@ describe User do
   it "is invalid without a password" do
   	expect(User.new(password: nil)).to have(1).errors_on(:password)
   end
+
+  it "is invalid with a password that is not 8 characters" do
+  	expect(User.new(email: "jon@jon.com", password: "1234567")).to have(1).errors_on(:password)
+  end
 end
