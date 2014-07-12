f94a4c8c097df4dcea48a918317adae896c71a04
Jonathan Chang <jonoschang7@gmail.com>
2014-07-12 14:35:17 -0400

    modify test for user sign up

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -9,9 +9,9 @@ feature 'User management' do
 		visit root_path
 		expect {
 			click_link 'Sign up'
-			fill_in 'user_email', with: @default.email
-			fill_in 'user_password', with: @default.password
-			fill_in 'user_password_confirmation', with: @default.password
+			fill_in 'user_email', with: "joe@joe.com"
+			fill_in 'user_password', with: "12345678"
+			fill_in 'user_password_confirmation', with: "12345678"
 			click_button 'Sign up'
 		}.to change(User, :count).by(1)
 	end
