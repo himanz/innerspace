ac4c81fbdb2b2e419580bb9760a30b33021f37b7
Jonathan Chang <jonoschang7@gmail.com>
2014-07-13 15:59:30 -0400

    route for logout with facebook

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Innerspace::Application.routes.draw do
   resources :categories
   devise_for :users
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  devise_scope :user do
+    get 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
