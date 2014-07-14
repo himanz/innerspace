2680bc720e74d3cbf6f58a07b55f42f582c7f865
Jonathan Chang <jonoschang7@gmail.com>
2014-07-14 14:02:26 -0400

    add york university and university of toronto to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,7 @@
 
 Category.create(name: "Hotel")
 Category.create(name: "Office")
+Category.create(name: "University")
 
 Business.create(name: "Sangri-La Hotel", 
 	              address: "188 University Ave", 
@@ -25,10 +26,16 @@ Business.create(name: "Search Engine People Inc.",
                 category_id: 2
                 )
 
-Business.create(name: "Ma-La Hotel", 
-	              address: "188 University Ave", 
-	              heading: 200, 
-	              pano: "3L5NCiIf__oAAAQIt-clrA",
-	              cbp: ",191.0,,0,-0.0",
-	              category_id: 1
-	              )
+Business.create(name: "York University",
+	              address: "4700 Keele St",
+	              heading: 200,
+	              pano: "2QJ-106Lsy2mP9jgSNUpGQ",
+	              cbp: "249.99,,0,1.8199997",
+	              category_id: 3)
+
+Business.create(name: "University of Toronto Scarborough",
+	              address: "1265 Military Trail",
+	              heading: 200,
+	              pano: "GWKQJ94LFA5mEjB_xrp7MQ",
+	              cbp: ",190.85,,0,4.75",
+	              category_id: 3)
