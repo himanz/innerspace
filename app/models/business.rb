11ec66a65f86bc13ac40944f34049edd8bf7cce9
Jonathan Chang <jonoschang7@gmail.com>
2014-07-15 15:26:22 -0400

    add latitude and longitude for business validation

--- app/models/business.rb
+++ app/models/business.rb
@@ -1,13 +1,7 @@
 class Business < ActiveRecord::Base
 	belongs_to :pin
 
-	validates :name, presence: true
-	validates :address, presence: true
-	validates :pano, presence: true
-	validates :heading, presence: true
-	validates :cbp, presence: true
-  validates :category_id, presence: true
-
+	validates :name, :address, :pano, :heading, :cbp, :category_id, :latitude, :longitude, presence: true
 
 	def self.by_letter(letter)
 		where("name LIKE ?", "#{letter}%").order(:name)
