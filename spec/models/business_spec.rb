39382685da5cfd7337a54e20591d4f9e2dff12af
Jonathan Chang <jonoschang7@gmail.com>
2014-07-15 12:35:39 -0400

    test for valid streetview static image url

--- spec/models/business_spec.rb
+++ spec/models/business_spec.rb
@@ -20,4 +20,9 @@ describe Business do
   it "is invalid without a pano" do
   	expect(Business.new(pano: nil)).to have(1).errors_on(:pano)
   end
+
+  it "returns a valid streetview static image link" do
+    business = create(:business)
+    expect(business.streetview_image).to eq "http://maps.googleapis.com/maps/api/streetview?size=500x300&heading=200.0&pitch=0&pano=3L5NCiIf__oAAAQIt-clrA&sensor=false&key=#{ENV['google_maps_api_key']}"
+  end
 end
