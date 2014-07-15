dac0bde5c7f36aa2714ceca6979e24ac7ab69c7c
Jonathan Chang <jonoschang7@gmail.com>
2014-07-15 14:04:43 -0400

    comment out section to include latitude, longitude in business model

--- app/models/business.rb
+++ app/models/business.rb
@@ -20,4 +20,8 @@ class Business < ActiveRecord::Base
 	def streetview_image
 		"http://maps.googleapis.com/maps/api/streetview?size=500x300&heading=#{heading}&pitch=0&pano=#{pano}&sensor=false&key=#{ENV['google_maps_api_key']}"
 	end
+
+	# def streetview_tour
+	# 	"https://maps.google.ca/maps?hl=en_uk&ll=#{business.latitude},#{business.longitude}&layer=c&panoid=#{business.pano}&cbp=#{business.cbp}&t=m&z=17&source=embed&output=svembed"
+	# end
 end
