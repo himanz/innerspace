0f62370a0431a3de5f1c57dd2f7b1e3828086e6e
Jonathan Chang <jonoschang7@gmail.com>
2014-07-15 17:10:20 -0400

    remove changes

--- spec/features/categories_spec.rb
+++ spec/features/categories_spec.rb
@@ -4,38 +4,36 @@ feature 'Category management' do
 	before :each do
 		@default = create(:default_category)
 	end
-  
-  shared_examples("full access to categories") do
-		scenario "add a new category" do
-			visit new_category_path
-			expect {
-				fill_in 'Name', with: @default.name
-				click_button 'Submit'
-			}.to change(Category, :count).by(1)
-			expect(current_path).to eq category_path(@default.id + 1)
-			expect(page).to have_content "Category was successfully created"
-			expect(page).to have_content 'Hotel'
-		end
 
-		scenario "edit a category" do
-			visit edit_category_path(@default)
-			expect{
-				fill_in 'Name', with: 'College'
-				click_button 'Submit'
-			}.to_not change(Category, :count)
-			expect(current_path).to eq category_path(@default)
-			expect(page).to have_content "Category was successfully updated"
-			expect(page).to have_content 'College'
-		end
+	scenario "add a new category" do
+		visit new_category_path
+		expect {
+			fill_in 'Name', with: @default.name
+			click_button 'Submit'
+		}.to change(Category, :count).by(1)
+		expect(current_path).to eq category_path(@default.id + 1)
+		expect(page).to have_content "Category was successfully created"
+		expect(page).to have_content 'Hotel'
+	end
 
-		scenario "delete a category" do
-			visit edit_category_path(@default)
-			expect{
-				click_link 'Delete'
-			}.to change(Category, :count).by(-1)
-			expect(current_path).to eq categories_path
-			expect(page).to have_content "Category was successfully deleted"
-			expect(page).to have_content "Index"
-		end
+	scenario "edit a category" do
+		visit edit_category_path(@default)
+		expect{
+			fill_in 'Name', with: 'College'
+			click_button 'Submit'
+		}.to_not change(Category, :count)
+		expect(current_path).to eq category_path(@default)
+		expect(page).to have_content "Category was successfully updated"
+		expect(page).to have_content 'College'
+	end
+
+	scenario "delete a category" do
+		visit edit_category_path(@default)
+		expect{
+			click_link 'Delete'
+		}.to change(Category, :count).by(-1)
+		expect(current_path).to eq categories_path
+		expect(page).to have_content "Category was successfully deleted"
+		expect(page).to have_content "Index"
 	end
 end
