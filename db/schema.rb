a188ce59b2543920b9dbc615b4017bafa43f9380
Jonathan Chang <jonoschang7@gmail.com>
2014-07-16 16:47:01 -0400

    add city to business model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140716191656) do
+ActiveRecord::Schema.define(version: 20140716204601) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20140716191656) do
     t.decimal  "longitude"
     t.string   "state"
     t.string   "country"
+    t.string   "city"
   end
 
   create_table "categories", force: true do |t|
