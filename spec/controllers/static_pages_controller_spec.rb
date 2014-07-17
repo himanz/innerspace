92d668e0f636bbd759cdfc5c7d185de66ef7054f
Jonathan Chang <jonoschang7@gmail.com>
2014-07-17 11:45:56 -0400

    test for about me

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -1,5 +1,10 @@
 require 'spec_helper'
 
 describe StaticPagesController do
-
+  describe "GET #about_me" do
+  	it "renders the about_me template" do
+  		get :about_me
+  		expect(response).to render_template :about_me
+  	end
+  end
 end
