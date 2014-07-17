823c69e8b7019d385b36897e9197d44ef3999809
Jonathan Chang <jonoschang7@gmail.com>
2014-07-17 19:29:46 -0400

    email works for contact us

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,4 +28,18 @@ Innerspace::Application.configure do
   config.assets.debug = true
 
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
+
+  config.action_mailer.perform_deliveries = true
+
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: ENV["GMAIL_DOMAIN"],
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV["GMAIL_USERNAME"],
+    password: ENV["GMAIL_PASSWORD"]
+  }
 end
