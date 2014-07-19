1cde0bacbabe791ae38f3578e550f56d94b99d46
Jonathan Chang <jonoschang7@gmail.com>
2014-07-18 21:50:53 -0400

    add image column to businesses table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140717191510) do
+ActiveRecord::Schema.define(version: 20140719015007) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,10 @@ ActiveRecord::Schema.define(version: 20140717191510) do
     t.string   "state"
     t.string   "country"
     t.string   "city"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "categories", force: true do |t|
