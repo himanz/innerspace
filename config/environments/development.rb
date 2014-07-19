97bd84cc952fa530c4a32c33e1debbdf08074fcc
Jonathan Chang <jonoschang7@gmail.com>
2014-07-19 01:43:50 -0400

    add aws keys to credentials

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -47,6 +47,8 @@ Innerspace::Application.configure do
     :storage => :3,
     :s3_credentials => {
       :bucket => 'innerspace'
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID']
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     }
   }
 end
