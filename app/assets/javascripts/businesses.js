58d063a5b77a8e119164834bb35f9e7fa70e362a
Jonathan Chang <jonoschang7@gmail.com>
2014-07-21 22:53:03 -0400

    on image hover, fades

---
+++ app/assets/javascripts/businesses.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+	
+	$(".streetview-image").mouseenter(function() {
+		$(this).fadeTo( "slow", 0.5)
+		})
+
+	$(".streetview-image").mouseleave(function() {
+  	$(this).fadeTo( "slow", 1)
+	})
+})
+
