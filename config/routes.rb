36420ceaaadb08e5be6cc60e6ae6bae9b40fbc82
Jonathan Chang <jonoschang7@gmail.com>
2014-07-23 14:19:20 -0400

    route for commontator

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,18 @@
 Innerspace::Application.routes.draw do
   root to: "businesses#index"
+
   resources :businesses do
     get :recent, on: :collection 
     get :random, on: :collection
   end
+
   resources :categories
+
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+
   get "about", :to => 'static_pages#about'
+  
+  mount Commontator::Engine => '/commontator'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
