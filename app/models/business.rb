b1467b4209972a13184963f6258d6544855e05a1
Jonathan Chang <jonoschang7@gmail.com>
2014-07-23 16:03:31 -0400

    add acts as commontable method to business model

--- app/models/business.rb
+++ app/models/business.rb
@@ -13,6 +13,8 @@ class Business < ActiveRecord::Base
 	validates :name, :address, :pano, :heading, :cbp, :category_id, :latitude, :longitude, :state, :country, :city, presence: true
   validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg"]
 
+  acts_as_commontable
+
   def image_remote_url
   	self.image = URI.parse(streetview_image)
   	@image_remote_url = streetview_image
