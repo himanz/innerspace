6e8ecfedbaeccb50a48ddad583b17603fcc0fdc5
Jonathan Chang <jonoschang7@gmail.com>
2014-07-27 15:01:43 -0400

    add mouseover and leave effects to country select

---
+++ app/assets/javascripts/static_pages.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+	
+	$(".country").mouseenter(function() {
+		$(this).fadeTo( "slow", 1)
+		})
+
+	$(".country").mouseleave(function() {
+  	$(this).fadeTo( "slow", 0.85)
+	})
+})
