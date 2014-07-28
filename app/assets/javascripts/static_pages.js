636742336a70bc60ba25958cfd93bb9567a82294
Jonathan Chang <jonoschang7@gmail.com>
2014-07-28 16:16:37 -0400

    location text center on mobile

--- app/assets/javascripts/static_pages.js
+++ app/assets/javascripts/static_pages.js
@@ -8,13 +8,5 @@ $(document).ready(function() {
   	$(this).fadeTo( "slow", 0.85)
 	})
 
-	  $(window).load(function() {
-
-	    if($(window).width()<481){
-	        $('#taj-video').get(0).pause();
-	        $('#taj-video').remove();
-	        $('#video-container').css("background", "url(chavaz.jpg");
-	    }
-	    
-	});
+	  
 })
