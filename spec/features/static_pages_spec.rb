35e446a30203f35c1e265bb701e28bde490f1531
Jonathan Chang <jonoschang7@gmail.com>
2014-08-01 13:37:42 -0400

    test for contact page

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -1,10 +1,17 @@
 require 'spec_helper'
 
 feature 'Static Pages link click' do
-	scenario "click about in nav bar" do
+	scenario "click about in footer" do
 		visit root_path
 		click_link 'About'
 		expect(current_path).to eq about_path
 		expect(page).to have_content 'About'
+	end
+
+	scenario "click contact in footer" do
+		visit businesses_path
+		click_link 'Contact'
+		expect(current_path).to eq contact_us_path
+		expect(page).to have_content "Contact Me"
 	end
 end
