af434f172947edcdcf9cf73e38d7d0b215b006cb
Jonathan Chang <jonoschang7@gmail.com>
2014-08-05 15:18:46 -0400

    cleaner formatting

--- app/assets/javascripts/businesses.js
+++ app/assets/javascripts/businesses.js
@@ -1,13 +1,10 @@
 $(document).ready(function() {
-	
 	$(".streetview-image").mouseenter(function() {
 		$(this).fadeTo( "fast", 0.5)
-		})
+	})
 
 	$(".streetview-image").mouseleave(function() {
   	$(this).fadeTo( "fast", 1)
-	})
-
-	
+	})	
 })
 
